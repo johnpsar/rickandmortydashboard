9a00bc62778dc3b892bb5fa61bcfc71eec087be7
johnpsar <jps200010@gmail.com>
2023-03-05 00:45:03 +0200

    trigger deploy gh-pages

--- src/components/Home/Card.vue
+++ src/components/Home/Card.vue
@@ -153,8 +153,8 @@ onMounted(async () => {
   .card-container {
     width: 300px;
     height: 200px;
-    background-color: #3c3e44;
     border-radius: 8px;
+    background-color: #3c3e44;
     box-shadow: rgb(0 0 0 / 10%) 0px 4px 6px -1px,
       rgb(0 0 0 / 6%) 0px 2px 4px -1px;
     display: flex;
